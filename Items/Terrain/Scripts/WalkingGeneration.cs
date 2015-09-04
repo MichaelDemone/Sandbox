4f083cfa79eeee331b97c72612ffef72ad0da8d5
Michael Demone <m_d_emone@hotmail.com>
2015-09-03 20:55:50 -0700

    Finished walking loading! FINALLY
    YAYYAYAYAY

---
+++ Items/Terrain/Scripts/WalkingGeneration.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingGeneration : CreateMap {
+	
+	//private static int playerXPosition;
+
+	// Loading
+	public static void loadPeices(int xPos) {
+
+		int playerXPosition = xPos;
+		bool goingRight = player.GetComponent<Rigidbody2D> ().velocity.x > 0;
+		
+		if(goingRight) {
+			int loadingPosition = playerXPosition + distanceBetweenLoads;
+			int loadTo = playerXPosition + 2*distanceBetweenLoads;
+
+			for (float i = loadingPosition; i < loadTo; i += widthOfGroundPiece) {
+				for  (float j = minY; j < maxY; j += widthOfGroundPiece) {
+					loadPeice(i,j,0);
+				}
+			}
+
+		} else {
+			int loadingPosition = playerXPosition - distanceBetweenLoads;
+			int loadTo = playerXPosition - 2*distanceBetweenLoads;
+
+			for (float i = loadingPosition; i > loadTo; i -= widthOfGroundPiece) {
+				for  (float j = minY; j < maxY; j += widthOfGroundPiece) {
+					loadPeice(i,j,0);
+				}
+			}
+		}
+	}
+	
+	public static void loadPeice(float i, float j, float k) {
+		Vector3 pos = new Vector3 (i, j, k);
+		string tile = (string) map[pos];
+		if(tile != null) {
+			GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), pos, Quaternion.identity);
+			obj.transform.SetParent(ground.transform);
+		}
+	}
+
+	// Unloading
+	public static void unloadPeices(int xPos) {
+
+		int playerXPosition = xPos;
+		bool goingRight = player.GetComponent<Rigidbody2D>().velocity.x > 0;
+
+		if(goingRight) {
+			float startingPos = playerXPosition - distanceBetweenLoads;
+			float endingPos = playerXPosition - 2*distanceBetweenLoads;
+
+			for (float i = startingPos; i > endingPos; i -= widthOfGroundPiece) {
+				for(float j = minY; j < maxY; j += widthOfGroundPiece) {
+					unloadPeice(i,j,0);
+				}
+			}
+		} else {
+			float startingPos = playerXPosition + distanceBetweenLoads;
+			float endingPos = playerXPosition + 2*distanceBetweenLoads;
+
+			for (float i = startingPos; i < endingPos; i += widthOfGroundPiece) {
+				for(float j = minY; j < maxY; j += widthOfGroundPiece) {
+					unloadPeice(i,j,0);
+				}
+			}
+		}
+	}
+
+	// Saves point to the map
+	public static void unloadPeice(float i, float j, float k) {
+		Vector3 pos = new Vector3 (i, j, k);
+		Collider2D box = Physics2D.OverlapPoint(pos);
+		
+		if(map.Contains(pos)) {
+			map.Remove(pos);
+		}
+		
+		Item script;
+		if(box != null && (script = box.gameObject.GetComponent<Item>()) != null) {
+			map.Add(pos, script.name);
+			GameObject.Destroy(box.gameObject);
+		}
+	}
+
+	// Are there blocks in a given line/area?
+	private static bool areBlocksAt(float beginXPos, float endXPos, float beginYPos, float endYPos) {
+		
+		Vector2 pos = new Vector2();
+		
+		for (float i = beginXPos; i < endXPos; i+= widthOfGroundPiece) {
+			for(float j = beginYPos; j < endYPos; j+= widthOfGroundPiece) {
+				pos.x = i;
+				pos.y = j;
+				if(Physics2D.OverlapPoint (pos) != null)
+					return true;
+			}
+		}
+		return false;
+	}
+}
--- StartMenu/CreateMap.cs
+++ StartMenu/CreateMap.cs
@@ -3,196 +3,90 @@ using System.Collections;
 
 public class CreateMap : MonoBehaviour {
 
-	public GameObject dirt, copper;
-	public int seed = 1234;
-	bool thing = false;
-	private int maxY = 0;
-	private float previousYPosR, previousYPosL;
-	private GameObject player, Ground;
 	public static Hashtable map;//the map
-	private float widthOfGroundPiece = 1;
+	public int seed = 1234;
+
+	protected static GameObject player, ground;
+	protected static float maxY = 0, minY = 0;
+	protected static float previousYPosR = 0, previousYPosL = 0;
+	protected static float widthOfGroundPiece = 1;
+	protected const float MINIMUM_Y = -1000, MINIMUM_GROUND_Y = 0;
+	protected static int distanceBetweenLoads = 5;
+	private int lastTransition = 0;
+	private bool rightLastTransition = false;
 
 	// Use this for initialization
 	void Start () {
 
-		Ground =  GameObject.Find("Ground");
+		ground =  GameObject.Find("Ground");
 		player = GameObject.Find ("Player");
-
+		player.transform.position = new Vector3 (-distanceBetweenLoads / 2, player.transform.position.y);
 		if (map == null) {
+
+			// Put the correct data in the map
+
 			map = new Hashtable ();
 			Random.seed = seed;
 
+			Vector3 position = new Vector3();
 			// This is the starting position
-			Vector3 position = new Vector3 (-1000, -20, 0);
+			previousYPosR = 10;
+			previousYPosL = 10;
 
-			previousYPosR = 0;
-
-			// Continue creating peices until the x position reaches 20
+			// Create map from -1000 to 1000
 			for (float i = -1000; i < 1000; i += widthOfGroundPiece) {
-			
-				int columnHeight = selectDirection ();
-				if (i == -1000)
-					previousYPosL = columnHeight;
-			
-				for (float j = -20; j < previousYPosR + columnHeight; j += widthOfGroundPiece) {
-					position = new Vector3 (i, j, 0);
-					//if(j % 5 == 0){
-					map.Add (position, "Dirt");
-					//}else{
-					//	map.Add(position,"Copper");
-					//}
 
-					thing = !thing;
-				
-					if (columnHeight + previousYPosR > maxY)
-						maxY += columnHeight;
-				
+				// Pick whether the block goes up or down
+				previousYPosR += selectDirection();
+
+				for (float j = -20; j < previousYPosR; j += widthOfGroundPiece) {
+					position = new Vector3 (i, j, 0);
+					map.Add (position, "Dirt");
 				}
+
+				// Put grassy block on top
 				map.Remove (position);
 				map.Add (position, "DirtWGrass");
-				previousYPosR += columnHeight;
+
+				if (previousYPosR > maxY) maxY = previousYPosR;
 			}
+
 		} else {
-			Debug.Log("Map is not null");
+			Debug.Log("Loading map");
 			maxY = 100;
 		}
 
-		for (float i = -199; i < 100; i += widthOfGroundPiece){
-			for (float j = -20; j < maxY; j += widthOfGroundPiece){
-				string tile = (string) map[new Vector3(i,j,0)];
-				if(tile != null) {
-					GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), new Vector3(i,j,0), Quaternion.identity);
-					obj.layer = LayerMask.NameToLayer("Ground");
-					obj.transform.SetParent(Ground.transform);
-				}
+		for (float i = -2*distanceBetweenLoads + widthOfGroundPiece; i < distanceBetweenLoads; i += widthOfGroundPiece){
+			for (float j = minY; j < maxY; j += widthOfGroundPiece){
+				WalkingGeneration.loadPeice(i,j,0);
 			}
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Mathf.RoundToInt (player.transform.position.x) % 100 == 0) {
-			StartCoroutine("loadPeices");
-			StartCoroutine("unloadPeices");
+		int xPos = Mathf.RoundToInt (player.transform.position.x);
+		if (xPos % distanceBetweenLoads == 0) {
+			bool goingRight = player.GetComponent<Rigidbody2D>().velocity.x > 0;
+
+			if(xPos == lastTransition && goingRight != rightLastTransition) {
+				WalkingGeneration.unloadPeices(xPos);
+				WalkingGeneration.loadPeices(xPos);
+			}
+			else if (xPos == lastTransition) {
+				return;
+			} else {
+				WalkingGeneration.unloadPeices(xPos);
+				WalkingGeneration.loadPeices(xPos);
+			}
+			rightLastTransition = goingRight;
+			lastTransition = xPos;
+
 		}
 	}
 
 	//adds or subtracts 1 or 2 blocks to the height
-	int selectDirection(){
-
-		int dir = (int) (Random.value * 100);
-
-		if (dir % 2 == 0){
-			int amnt = Random.Range(0,2);
-			return amnt;
-		} else {
-			int amnt = Random.Range(0,2);
-			return (-1 * amnt);
-		}
-	}
-
-	public IEnumerator loadPeices() {
-
-		int playerXPosition = Mathf.RoundToInt(player.transform.position.x);
-		bool goingRight;
-		if (player.name.Equals ("Player2")) {
-			goingRight = player.GetComponent<PlayerControl> ().goingRight;
-		} else {
-			goingRight = player.GetComponent<Rigidbody2D> ().velocity.x > 0;
-		}
-		Vector3 position;
-
-		if(goingRight) {
-
-			position = new Vector3 (playerXPosition + 100,0,0);
-
-			for(float i = -40; i < 20; i += widthOfGroundPiece) {
-				Collider2D box = Physics2D.OverlapPoint(new Vector2(position.x, i));
-				if(box != null) {
-					yield break;
-				}
-			}
-
-			for (float i = playerXPosition + 100; i < playerXPosition + 200; i += widthOfGroundPiece) {
-				position.x = i;
-				previousYPosR += selectDirection ();
-
-				for  (float j = -20; j < maxY; j += widthOfGroundPiece) {
-					position.y = j;
-
-					string tile = (string) map[new Vector3(i,j,0)];
-					if(tile != null) {
-						GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), position, Quaternion.identity);
-						obj.transform.SetParent(Ground.transform);
-					}
-				}
-			}
-		} else {
-			
-			position = new Vector3 (playerXPosition - 100,0,0);
-
-			for(float i = -40; i < 20; i += widthOfGroundPiece) {
-				Collider2D box = Physics2D.OverlapPoint(new Vector2(position.x, i));
-				if(box != null) {
-					yield break;
-				}
-			}
-
-			for (float i = playerXPosition - 100; i > playerXPosition - 200; i -= widthOfGroundPiece) {
-				position.x = i;
-
-				previousYPosL += selectDirection ();
-				
-				for  (float j = -20; j < maxY; j += widthOfGroundPiece) {
-					position.y = j;
-
-					string tile = (string) map[position];
-					if(tile != null) {
-						GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), position, Quaternion.identity);
-						obj.transform.SetParent(Ground.transform);
-					}
-				}
-				
-			}
-		}
-		yield return null;
-	}
-
-
-
-	public IEnumerator unloadPeices() {
-
-		int playerXPosition = Mathf.RoundToInt(player.transform.position.x);
-		bool goingRight = player.GetComponent<Rigidbody2D>().velocity.x > 0;
-
-		if(goingRight) {
-			for (float i = playerXPosition - 100; i > playerXPosition - 200; i -= widthOfGroundPiece) {
-				for(float j = -40; j < 300; j += widthOfGroundPiece) {
-					savePointToMap(i,j,0);
-				}
-			}
-		} else {
-			for (float i = playerXPosition + 100; i < playerXPosition + 200; i += widthOfGroundPiece) {
-				for(float j = -20; j < 300; j += widthOfGroundPiece) {
-					savePointToMap(i,j,0);
-				}
-			}
-		}
-		yield return null;
-	}
-
-	private void savePointToMap(float i, float j, float k) {
-		Vector3 pos = new Vector3 (i, j, k);
-		Collider2D box = Physics2D.OverlapPoint(pos);
-		
-		if(map.Contains(pos)) {
-			map.Remove(pos);
-		}
-
-		Item script;
-		if(box != null && (script = box.gameObject.GetComponent<Item>()) != null) {
-			map.Add(pos, script.name);
-			GameObject.Destroy(box.gameObject);
-		}
+	protected int selectDirection() {
+		return (int) Random.Range (-1.95f, 1.95f);
 	}
 }
