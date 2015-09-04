3f201b3262fcf9ba675e47ae6fd69334d5f7f00a
DrProfesor <jwvandervliet@Hotmail.com>
2015-09-04 00:57:18 -0700

    Fixed Create Map

--- StartMenu/CreateMap.cs
+++ StartMenu/CreateMap.cs
@@ -33,30 +33,18 @@ public class CreateMap : MonoBehaviour {
 
 			Vector3 position = new Vector3();
 			// This is the starting position
-<<<<<<< HEAD
 			previousYPosR = 10;
 			previousYPosL = 10;
-=======
-			Vector3 position = new Vector3 (-1000, -40, 0);
->>>>>>> origin/master
+
 
 			// Create map from -1000 to 1000
 			for (float i = -1000; i < 1000; i += widthOfGroundPiece) {
-<<<<<<< HEAD
 
 				// Pick whether the block goes up or down
 				previousYPosR += selectDirection();
 
 				for (float j = -20; j < previousYPosR; j += widthOfGroundPiece) {
-=======
-			
-				int columnHeight = selectDirection ();
-				Debug.Log(columnHeight);
-				if (i == -1000)
-					previousYPosL = columnHeight;
-			
-				for (float j = -40; j < previousYPosR + columnHeight; j += widthOfGroundPiece) {
->>>>>>> origin/master
+
 					position = new Vector3 (i, j, 0);
 					map.Add (position, "Dirt");
 				}
@@ -68,30 +56,20 @@ public class CreateMap : MonoBehaviour {
 				if (previousYPosR > maxY) maxY = previousYPosR;
 			}
 
-<<<<<<< HEAD
-=======
+
 			//GenerateTrees.placeTrees(maxY);
 
->>>>>>> origin/master
 		} else {
 			Debug.Log("Loading map");
 			maxY = 100;
 		}
 
-<<<<<<< HEAD
+
+
 		for (float i = -2*distanceBetweenLoads + widthOfGroundPiece; i < distanceBetweenLoads; i += widthOfGroundPiece){
 			for (float j = minY; j < maxY; j += widthOfGroundPiece){
 				WalkingGeneration.loadPeice(i,j,0);
-=======
-		for (float i = -199; i < 100; i += widthOfGroundPiece){
-			for (float j = -40; j < maxY; j += widthOfGroundPiece){
-				string tile = (string) map[new Vector3(i,j,0)];
-				if(tile != null) {
-					GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), new Vector3(i,j,0), Quaternion.identity);
-					obj.layer = LayerMask.NameToLayer("Ground");
-					obj.transform.SetParent(Ground.transform);
-				}
->>>>>>> origin/master
+
 			}
 		}
 	}
@@ -106,58 +84,17 @@ public class CreateMap : MonoBehaviour {
 				WalkingGeneration.unloadPeices(xPos);
 				WalkingGeneration.loadPeices(xPos);
 			}
-<<<<<<< HEAD
 			else if (xPos == lastTransition) {
 				return;
 			} else {
 				WalkingGeneration.unloadPeices(xPos);
 				WalkingGeneration.loadPeices(xPos);
-=======
 
-			for (float i = playerXPosition + 100; i < playerXPosition + 200; i += widthOfGroundPiece) {
-				position.x = i;
-				previousYPosR += selectDirection ();
-
-				for  (float j = -40; j < maxY; j += widthOfGroundPiece) {
-					position.y = j;
-
-					string tile = (string) map[new Vector3(i,j,0)];
-					if(tile != null) {
-						GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), position, Quaternion.identity);
-						obj.transform.SetParent(Ground.transform);
-					}
-				}
->>>>>>> origin/master
 			}
 			rightLastTransition = goingRight;
 			lastTransition = xPos;
 
-<<<<<<< HEAD
-=======
-			for(float i = -40; i < 20; i += widthOfGroundPiece) {
-				Collider2D box = Physics2D.OverlapPoint(new Vector2(position.x, i));
-				if(box != null) {
-					yield break;
-				}
-			}
 
-			for (float i = playerXPosition - 100; i > playerXPosition - 200; i -= widthOfGroundPiece) {
-				position.x = i;
-
-				previousYPosL += selectDirection ();
-				
-				for  (float j = -40; j < maxY; j += widthOfGroundPiece) {
-					position.y = j;
-
-					string tile = (string) map[position];
-					if(tile != null) {
-						GameObject obj = (GameObject) GameObject.Instantiate(Dictionary.get (tile), position, Quaternion.identity);
-						obj.transform.SetParent(Ground.transform);
-					}
-				}
-				
-			}
->>>>>>> origin/master
 		}
 	}
 
