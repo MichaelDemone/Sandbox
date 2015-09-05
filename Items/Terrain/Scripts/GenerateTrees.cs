1791c0a50e6cf0e5a0b29e3acc2e00cb58a463d3
DrProfesor <jwvandervliet@Hotmail.com>
2015-09-04 23:32:03 -0700

    Basic Trees

--- Items/Terrain/Scripts/GenerateTrees.cs
+++ Items/Terrain/Scripts/GenerateTrees.cs
@@ -3,11 +3,9 @@ using System.Collections;
 
 public class GenerateTrees {
 
-	public static void placeTrees(int maxY, float biomeProb){
+	public static void treeLocation(int maxY, float biomeProb){
 
 		int counter = 0;
-
-
 
 		for (int i = -1000; i < 1000; i++){
 			for (int j = 0; j < maxY; j++){
@@ -16,7 +14,7 @@ public class GenerateTrees {
 				if (currentTile != null){
 					if (currentTile.Equals("DirtWGrass")  &&  counter > 5){
 						if((int) Random.Range (-1.95f, biomeProb) == 1){
-							CreateMap.map.Add(new Vector3(i,j+1,0), "Log");
+							placeTree(new Vector3(i,j+1,0));
 							counter = 0;
 						}
 					}else if (currentTile.Equals ("DirtWGrass")){
@@ -25,5 +23,29 @@ public class GenerateTrees {
 				}
 			}
 		}
+	}
+
+	public static void placeTree(Vector3 location){
+
+		CreateMap.map.Add(location, "Log");
+		CreateMap.map.Add(new Vector3(location.x,location.y+1,location.z), "Log");
+		CreateMap.map.Add(new Vector3(location.x,location.y+2,location.z), "Log");
+		CreateMap.map.Add(new Vector3(location.x,location.y+3,location.z), "Log");
+		CreateMap.map.Add(new Vector3(location.x,location.y+4,location.z), "Log");
+
+		CreateMap.map.Add(new Vector3(location.x-1,location.y+3,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x+1,location.y+3,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x-2,location.y+3,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x+2,location.y+3,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x-3,location.y+3,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x+3,location.y+3,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x-1,location.y+4,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x+1,location.y+4,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x-2,location.y+4,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x+2,location.y+4,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x,location.y+5,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x-1,location.y+5,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x+1,location.y+5,location.z), "Leaves");
+		CreateMap.map.Add(new Vector3(location.x,location.y+6,location.z), "Leaves");
 	}
 }
